1d5959fdfc07bc7ae39b8066155649df6e7b70ee
Roman <mmkenq@gmail.com>
2023-11-12 17:26:32 +0400

    players history

--- js/components/Graph2D/UI/Graph2DUIComponent.js
+++ js/components/Graph2D/UI/Graph2DUIComponent.js
@@ -1,10 +1,16 @@
-const SECS = 60;
+const TIMEZONE_OFFSET = 4;
+const SECS = 4;
 
 class Graph2DUIComponent extends Component {
 	ballsHeader = null;
 	ballsCount = 0;
 	timeHeader = null;
 	timerSecs = SECS;
+	playersHeader = null;
+	playersList = null;
+	players = [
+//		{time: 0, balls: 0},
+	];
 
 	updateBalls(count){		
 		this.ballsCount = count;
@@ -22,6 +28,28 @@ class Graph2DUIComponent extends Component {
 		this.timeHeader.innerHTML = 'TIME: ' + result;
 	}
 
+	addPlayer(timestamp, duration, balls){
+		this.players.push({timestamp: timestamp, duration: duration, balls: balls});
+		this.updatePlayers(this.players);
+	}
+	updatePlayers(players){
+		players.sort((p1,p2)=>{
+			if(p1.balls < p2.balls) return 1;
+			if(p1.balls > p2.balls) return -1;
+			return 0;
+
+		});
+		
+		if(this.playersList) this.playersList.remove();
+		this.playersList = document.createElement('ol');
+		players.forEach((p, i)=>{
+			let li = document.createElement('li');
+			li.innerHTML = 'timestamp: ' + p.timestamp + ' duration: ' + p.duration + 's balls: ' + p.balls;
+			this.playersList.appendChild(li);
+		});
+		this.playersHeader.appendChild(this.playersList);
+	}
+
 
 	createHTML(){
 		// BALLS HEADER
@@ -33,6 +61,11 @@ class Graph2DUIComponent extends Component {
 		this.timeHeader = document.createElement('div');
 		document.getElementById('infoPanel').appendChild(this.timeHeader);
 		this.updateTime(SECS);
+
+		// PLAYERS
+		this.playersHeader = document.createElement('div');
+		this.playersHeader.innerHTML = 'PLAYERS:';
+		document.getElementById('infoPanel').appendChild(this.playersHeader);
 
 		// TODO: BUTTONS
 	}
@@ -56,15 +89,19 @@ class Graph2DUIComponent extends Component {
 					this.updateTime(this.timerSecs);
 				}
 				else if(this.timerSecs==0){
-					if(interval) { clearInterval(interval);}		
+					pauseBut.dispatchEvent(new Event('click'));
 					interval = 0;
-					alert("TIMEOVER")
+						let today = new Date();
+						let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+						let time = ("0" + (today.getHours()+TIMEZONE_OFFSET)).slice(-2) + ":" +
+									("0" + today.getMinutes()).slice(-2) + ":" +
+									("0" + today.getSeconds()).slice(-2);
+						let dateTime = date+' '+time;
+						this.addPlayer(dateTime, SECS - this.timerSecs, this.ballsCount);
 					this.timerSecs = SECS;
 					this.ballsCount = 0;
 					this.updateTime(this.timerSecs);
 					this.updateBalls(this.ballsCount);
-					pauseBut.remove();
-					document.getElementById('buts2d').prepend(playBut);
 				}
 			}, 1000);
 			playBut.remove();
